635e092e0f76e9d58a86093810c4c7fa87ef3785
Paweł Leśnik <pawel.lesnik@tdy.pl>
2023-10-19 22:35:00 +0200

    add connent with graphCms

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,27 @@
-export default function Blog() {
+import { gql } from "@apollo/client";
+import { getClient } from "../../lib/client";
+const query = gql`
+	query GetAllPosts {
+		post(where: { slug: "murmure-splendidus-invidia-merui-usus-flammas-clamoribus" }) {
+			id
+			title
+			tags
+			publishedAt
+			blogContent {
+				html
+			}
+		}
+	}
+`;
+
+export default async function Blog() {
+	const { data } = await getClient().query({ query });
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<section>Blog</section>
+			<section>
+				<div dangerouslySetInnerHTML={{ __html: data.post.blogContent.html }} />
+			</section>
 		</main>
 	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import { ApolloWrapper } from "../lib/apollo-wrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -12,7 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="pl">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<ApolloWrapper>{children}</ApolloWrapper>
+			</body>
 		</html>
 	);
 }
