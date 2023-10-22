0a6f6f2c002e6c3d54e719db26b38367aaa5591f
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-10-22 23:56:04 +0200

    add link to post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 import Image from "next/image";
+import Link from "next/link";
 import { type GetAllPostsQuery, GetAllPostsDocument } from "../../gql/graphql";
 import { getClient } from "../../lib/client";
-
 export default async function Blog() {
 	const { data } = await getClient().query<GetAllPostsQuery>({
 		query: GetAllPostsDocument,
@@ -12,19 +12,20 @@ export default async function Blog() {
 	// const postHtml = data?.post?.blogContent?.html;
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>
-				{posts.map((post) => (
-					<div key={post.id}>
-						<h2>{post.title}</h2>
-						<Image
-							src={post.coverImage.coverImagePost[0]?.coverImage.url ?? ""}
-							layout="fill"
-							alt={post.title}
-						/>
-					</div>
-				))}
-			</div>
+		<main className="flex min-h-screen flex-col items-center gap-10 p-8">
+			{posts.map((post) => (
+				<Link href={`/blog/${post.slug}`} key={post.id}>
+					<h2 className="mb-4">{post.title}</h2>
+					<Image
+						className="w-4/4 block h-auto"
+						src={post.coverImage.coverImagePost[0]?.coverImage.url ?? ""}
+						width={1000}
+						height={500}
+						layout="cover"
+						alt={post.title}
+					/>
+				</Link>
+			))}
 		</main>
 	);
 }
--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,7 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "query GetPostBySlug($slug: String!) {\n  post(where: {slug: $slug}) {\n    id\n    title\n    tags\n    publishedAt\n    blogContent {\n      html\n    }\n  }\n}\n\nquery GetAllPosts {\n  posts {\n    title\n    tags\n    id\n    coverImage {\n      id\n      coverImagePost {\n        coverImage {\n          fileName\n          url\n        }\n      }\n    }\n  }\n}": types.GetPostBySlugDocument,
+    "query GetPostBySlug($slug: String!) {\n  post(where: {slug: $slug}) {\n    id\n    title\n    tags\n    publishedAt\n    blogContent {\n      html\n    }\n  }\n}\n\nquery GetAllPosts {\n  posts {\n    title\n    tags\n    id\n    slug\n    coverImage {\n      id\n      coverImagePost {\n        coverImage {\n          fileName\n          url\n        }\n      }\n    }\n  }\n}": types.GetPostBySlugDocument,
 };
 
 /**
@@ -33,7 +33,7 @@ export function gql(source: string): unknown;
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "query GetPostBySlug($slug: String!) {\n  post(where: {slug: $slug}) {\n    id\n    title\n    tags\n    publishedAt\n    blogContent {\n      html\n    }\n  }\n}\n\nquery GetAllPosts {\n  posts {\n    title\n    tags\n    id\n    coverImage {\n      id\n      coverImagePost {\n        coverImage {\n          fileName\n          url\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query GetPostBySlug($slug: String!) {\n  post(where: {slug: $slug}) {\n    id\n    title\n    tags\n    publishedAt\n    blogContent {\n      html\n    }\n  }\n}\n\nquery GetAllPosts {\n  posts {\n    title\n    tags\n    id\n    coverImage {\n      id\n      coverImagePost {\n        coverImage {\n          fileName\n          url\n        }\n      }\n    }\n  }\n}"];
+export function gql(source: "query GetPostBySlug($slug: String!) {\n  post(where: {slug: $slug}) {\n    id\n    title\n    tags\n    publishedAt\n    blogContent {\n      html\n    }\n  }\n}\n\nquery GetAllPosts {\n  posts {\n    title\n    tags\n    id\n    slug\n    coverImage {\n      id\n      coverImagePost {\n        coverImage {\n          fileName\n          url\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query GetPostBySlug($slug: String!) {\n  post(where: {slug: $slug}) {\n    id\n    title\n    tags\n    publishedAt\n    blogContent {\n      html\n    }\n  }\n}\n\nquery GetAllPosts {\n  posts {\n    title\n    tags\n    id\n    slug\n    coverImage {\n      id\n      coverImagePost {\n        coverImage {\n          fileName\n          url\n        }\n      }\n    }\n  }\n}"];
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {};
