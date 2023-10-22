864f892e54bd41ac5db3e3537a141e5c9a03ed36
Paweł Leśnik <pawel.lesnik@tdy.pl>
2023-10-22 15:20:05 +0200

    add post query

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,19 @@
-export default function BlogPost({ params }: { params: { slug: string } }) {
-	return <div>{params.slug}</div>;
+import { gql } from "@apollo/client";
+import { type GetPostBySlugQuery, GetPostBySlugDocument } from "../../../gql/graphql";
+
+import { getClient } from "../../../lib/client";
+
+export default async function BlogPost({ params }: { params: { slug: string } }) {
+	const { data } = await getClient().query<GetPostBySlugQuery>({
+		query: GetPostBySlugDocument,
+		variables: { slug: params.slug },
+	});
+
+	const postHtml = data?.post?.blogContent?.html;
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<section>{postHtml && <div dangerouslySetInnerHTML={{ __html: postHtml }} />}</section>
+		</main>
+	);
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,34 +1,29 @@
 import { gql } from "@apollo/client";
+import { type GetAllPostsQuery, GetAllPostsDocument } from "../../gql/graphql";
 
 import { getClient } from "../../lib/client";
 
-import { type GetPostBySlugQuery } from "../../gql/graphql";
-
-const GET_POST_BY_SLUG = gql`
-	query GetPostBySlug($slug: String!) {
-		post(where: { slug: $slug }) {
-			id
-			title
-			tags
-			publishedAt
-			blogContent {
-				html
-			}
-		}
-	}
-`;
-
 export default async function Blog() {
-	const { data } = await getClient().query<GetPostBySlugQuery>({
-		query: GET_POST_BY_SLUG,
-		variables: { slug: "murmure-splendidus-invidia-merui-usus-flammas-clamoribus" },
+	const { data } = await getClient().query<GetAllPostsQuery>({
+		query: GetAllPostsDocument,
 	});
+	const posts = data.posts;
 
-	const postHtml = data?.post?.blogContent?.html;
+	// const postHtml = data?.post?.blogContent?.html;
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<section>{postHtml && <div dangerouslySetInnerHTML={{ __html: postHtml }} />}</section>
+			<div>
+				{posts.map((post) => (
+					<div key={post.id}>
+						{" "}
+						{/* Dodaj klucz "key" */}
+						<div>{post.title}</div>
+						{/* Dodaj tag <img> do wyświetlenia obrazka */}
+						<img src={post.coverImage.coverImagePost[0]?.coverImage.url} alt={post.title} />
+					</div>
+				))}
+			</div>
 		</main>
 	);
 }
