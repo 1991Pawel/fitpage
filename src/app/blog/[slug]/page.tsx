2581498616d14bbe2efd1450b28f5d87b006b515
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-11-01 22:57:06 +0100

    add metadata to post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,19 @@
 import { getClient } from "@/lib/client";
 import { type GetPostBySlugQuery, GetPostBySlugDocument } from "@/gql/graphql";
+export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
+	const { data } = await getClient().query<GetPostBySlugQuery>({
+		query: GetPostBySlugDocument,
+		variables: { slug: params.slug },
+	});
 
+	return {
+		title: data?.post?.title,
+		descritpion: "description test",
+		alternates: {
+			canonical: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/blog/${params.slug}`,
+		},
+	};
+};
 export default async function BlogPost({ params }: { params: { slug: string } }) {
 	const { data } = await getClient().query<GetPostBySlugQuery>({
 		query: GetPostBySlugDocument,
@@ -11,6 +24,7 @@ export default async function BlogPost({ params }: { params: { slug: string } })
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<img src={data.post?.coverImage.url} alt={data.post?.title} />
 			<section>{postHtml && <div dangerouslySetInnerHTML={{ __html: postHtml }} />}</section>
 		</main>
 	);
