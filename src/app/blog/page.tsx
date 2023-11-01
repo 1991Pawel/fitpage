5882622bd7438a395a8ac698353beb5ae4c822f6
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-11-01 20:21:50 +0100

    remove instagram fetch

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,22 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { type GetAllPostsQuery, GetAllPostsDocument } from "@/gql/graphql";
 import { getClient } from "@/lib/client";
-import { getInstagramData } from "@/services";
 
 export default async function Blog() {
 	const { data } = await getClient().query<GetAllPostsQuery>({
 		query: GetAllPostsDocument,
 	});
 	const posts = data.posts;
-	// const instagramData = await getInstagramData();
 
 	return (
 		<main className="flex min-h-screen flex-col items-center gap-10 p-8">
-			{/* {JSON.stringify(instagramData?.data.data)} */}
-
 			{posts.map((post) => (
 				<Link href={`/blog/${post.slug}`} key={post.id}>
 					<h2 className="mb-4">{post.title}</h2>
+
 					<Image
 						className="w-4/4 block h-auto"
 						src={post.coverImage.coverImagePost[0]?.coverImage.url ?? ""}
