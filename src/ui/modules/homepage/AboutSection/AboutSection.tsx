0d2046003d359c359478516b5895c8d6f44c2bc7
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-12-16 11:30:10 +0100

    add

--- src/ui/modules/homepage/AboutSection/AboutSection.tsx
+++ src/ui/modules/homepage/AboutSection/AboutSection.tsx
@@ -1,6 +1,8 @@
 import style from "./aboutSection.module.css";
 import { Container } from "@/ui/components/Container/Contianer";
 import { Paragraph } from "@/ui/components/Paragraph/Paragraph";
+import Arrow from "@/ui/svgs/disclaimer-arrow.svg";
+import Image from "next/image";
 
 export const AboutSection = () => (
 	<section className={style.about}>
@@ -11,12 +13,35 @@ export const AboutSection = () => (
 					osoby, z którą współpracuje zarówno w aspekcie fizycznym, zdrowotnym jak i mentalnym.
 				</Paragraph>
 				<Paragraph>
-					Podczas trwania naszej współpracy osiągniecie Twojego celu jest moim priorytetem. Przy
-					pomocy dostępnego sprzętu w siłowni CityFit w Białymstoku, nauczę Cię poprawnie wykonywać
-					ćwiczenia, dzięki którym efektywnie zmienimy wygląd Twojej sylwetki.Zapraszam na treningi
-					personalne!
+					Podczas trwania naszej współpracy osiągniecie Twojego celu jest moim priorytetem.
+					Przy&nbsp;pomocy dostępnego sprzętu na&nbsp;siłowni CityFit w&nbsp;Białymstoku, nauczę Cię
+					poprawnie wykonywać ćwiczenia, dzięki którym efektywnie zmienimy wygląd Twojej sylwetki.
+					Zapraszam na&nbsp;treningi personalne!
 				</Paragraph>
+				<div className={style.disclaimer}>
+					<span>
+						<Arrow />
+						Widzimy się
+					</span>
+					na treningu!
+				</div>
 			</Container>
+		</div>
+		<div className={style.aboutGallery}>
+			<Image
+				src="/assets/home/about-01.jpg"
+				layout="responsive"
+				width={380}
+				height={680}
+				alt="Dawid Ciborowski"
+			/>
+			<Image
+				src="/assets/home/about-02.jpg"
+				layout="responsive"
+				width={380}
+				height={680}
+				alt="Dawid Ciborowski"
+			/>
 		</div>
 	</section>
 );
