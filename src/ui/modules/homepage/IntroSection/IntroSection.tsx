60513792660039a6ac1f214833c5592920cb134c
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-12-19 13:47:21 +0100

    add fn to animation

--- src/ui/modules/homepage/IntroSection/IntroSection.tsx
+++ src/ui/modules/homepage/IntroSection/IntroSection.tsx
@@ -12,65 +12,68 @@ export const IntroSection = () => {
 	const imageWrapper = useRef<HTMLDivElement | null>(null);
 
 	useEffect(() => {
-		if (contentWrapper?.current?.children) {
-			const [special, title, description, cta] = contentWrapper.current.children;
-			const overlayElement = overlay.current;
-			const imageElement = imageWrapper.current;
+		const runAnimation = () => {
+			if (contentWrapper?.current?.children) {
+				const [special, title, description, cta] = contentWrapper.current.children;
+				const overlayElement = overlay.current;
+				const imageElement = imageWrapper.current;
 
-			if (overlayElement) {
-				gsap.set(overlayElement, {
-					autoAlpha: 1,
-				});
-
-				const overlayTimeline = gsap.timeline({
-					defaults: {
-						ease: "power3.inOut",
-					},
-				});
-
-				overlayTimeline.fromTo(
-					overlayElement,
-					{ x: "0%" },
-					{
-						duration: 2,
-						x: "-100%",
+				if (overlayElement) {
+					gsap.set(overlayElement, {
 						autoAlpha: 1,
-					},
-				);
+					});
 
-				const imageWrapperTimeLine = gsap.timeline({
-					defaults: {
-						ease: "power3.inOut",
-					},
-				});
-				imageWrapperTimeLine.fromTo(
-					imageElement,
-					{ x: "-=15", autoAlpha: 0 },
-					{
-						duration: 1,
-						x: "0%",
-						autoAlpha: 1,
-						delay: 0.4,
-					},
-				);
+					const overlayTimeline = gsap.timeline({
+						defaults: {
+							ease: "power3.inOut",
+						},
+					});
 
-				const elementsTimeline = gsap.timeline({
-					defaults: {
-						ease: "power1.inOut",
-					},
-				});
-				elementsTimeline.fromTo(
-					[special, title, description, cta],
-					{ y: "-=15", autoAlpha: 0 },
-					{
-						delay: 1.2,
-						duration: 1,
-						y: "0%",
-						autoAlpha: 1,
-					},
-				);
+					overlayTimeline.fromTo(
+						overlayElement,
+						{ x: "0%" },
+						{
+							duration: 2,
+							x: "-100%",
+							autoAlpha: 1,
+						},
+					);
+
+					const imageWrapperTimeLine = gsap.timeline({
+						defaults: {
+							ease: "power3.inOut",
+						},
+					});
+					imageWrapperTimeLine.fromTo(
+						imageElement,
+						{ x: "-=15", autoAlpha: 0 },
+						{
+							duration: 1,
+							x: "0%",
+							autoAlpha: 1,
+							delay: 0.4,
+						},
+					);
+
+					const elementsTimeline = gsap.timeline({
+						defaults: {
+							ease: "power1.inOut",
+						},
+					});
+					elementsTimeline.fromTo(
+						[special, title, description, cta],
+						{ y: "-=15", autoAlpha: 0 },
+						{
+							delay: 1.2,
+							duration: 1,
+							y: "0%",
+							autoAlpha: 1,
+						},
+					);
+				}
 			}
-		}
+		};
+		runAnimation();
 	}, [contentWrapper, overlay]);
 	return (
 		<section className={style.intro}>
