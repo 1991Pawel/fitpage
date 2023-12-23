63d0b25dfca4379ea92e0d4d36b5fcbf75386770
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2023-12-23 22:54:12 +0100

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 import "./globals.css";
 
+import Script from "next/script";
 import { Montserrat } from "next/font/google";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
 import { Header } from "@/ui/components/Header/Header";
 import { Footer } from "@/ui/components/Footer/Footer";
 import { FacebookPixel } from "@/ui/components/FacebookPixel/FacebookPixel";
-import { Analytics } from "@/ui/components/Analytics/Analytics";
 const openSans = Montserrat({
 	subsets: ["latin"],
 	display: "swap",
@@ -16,6 +16,22 @@ const openSans = Montserrat({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html className={openSans.className} lang="pl">
+			<Script
+				id="ga_01"
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}`}
+			/>
+
+			<Script id="ga_02" strategy="lazyOnload">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</Script>
 			<body>
 				<div className="layout">
 					<Header />
@@ -24,7 +40,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				</div>
 				<FacebookPixel />
 			</body>
-			<Analytics />
 		</html>
 	);
 }
