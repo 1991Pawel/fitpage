1ccb6b41a14107197c022c79b0b3b10f6f2d81c6
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2024-01-01 17:03:18 +0100

    change config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
+const { redirect } = require("next/dist/server/api-utils");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		domains: ["https://fitpage-ten.vercel.app", "media.graphassets.com"],
 		formats: ["image/avif", "image/webp"],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/bezplatna-konsultacja",
+				destination: "/#kontakt",
+				permanent: true,
+			},
+		];
 	},
 
 	webpack(config) {
